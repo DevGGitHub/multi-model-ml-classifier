0a1c6e351a5bb4e88e5f9ba41e8a59c4a592df01
DevGGitHub <buddhadev.ganguly87@gmail.com>
2026-01-27 00:55:22 +0530

    modified the random forest files

--- model/random_forest.py
+++ model/random_forest.py
@@ -2,6 +2,10 @@ from sklearn.ensemble import RandomForestClassifier
 
 def build_model():
     return RandomForestClassifier(
-        n_estimators=100,
-        random_state=42
+        n_estimators=50,
+        max_depth=12,
+        min_samples_leaf=20,
+        max_features="sqrt",
+        random_state=42,
+        n_jobs=-1
     )
